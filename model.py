67ad05d81b6c7b49fa2de2df738e55fbfb7c8afc
Becca Saines <rebecca.saines@gmail.com>
2016-10-31 17:46:31 -0700

    Got specifics out of the Etsy API.  Added some login logic.  Added a few html pages.

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
-model.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_to_db(app):
+    """Connect to db"""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///TBD' # <-- CREATE A DB!!!
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
+
+connect_to_db(app)
--- server.py
+++ server.py
@@ -1,1 +1,51 @@
-server.py
+from flask import Flask, render_template, jsonify, request, session
+import requests
+import os
+
+app = Flask(__name__)
+
+app.secret_key = "talknerdytome"
+SHARED_SECRET = os.environ["SHARED_SECRET"]
+KEYSTRING = os.environ["KEYSTRING"]
+
+# ROUTES
+# *************************************************
+
+@app.route('/')
+def index():
+    """Shnerdy introduction (need), login/OAuth form (need), logout button if user not in session (need)"""
+
+    print "\n\n\n\n****************\nTHIS IS THE SESSION: ", session, "\n\n\n"
+    access_token = session.get('access_token')
+    print "\n\n\n ACCESS TOKEN: ", access_token, "\n\n\n"
+
+    # Logoout or login only shows if the user is logged in
+    if access_token is None:
+        user_sess = None
+
+    return render_template('index.html', user_sess=user_sess)
+
+@app.route('/etsy_listings.json')
+def get_some_stuff():
+    """Hoping to return shtuff from the etsy API"""
+
+    search_term = 'Dorothy Gale'
+
+    url = 'https://openapi.etsy.com/v2/listings/active?api_key=' + KEYSTRING
+
+    payload = {'tags' : search_term}
+    print payload
+
+    response = requests.get(url, params=payload)
+    json_response = response.json()
+
+    for item in json_response["results"]:
+        print "\nTitle: ", item["title"], "\nListing ID: ", item["listing_id"], "\nPrice: ", item["price"], "\nURL: ", item["url"], "\nDigital: ", item["is_digital"], "\nCategory: ", item["category_path"]
+
+    return jsonify(json_response)
+
+
+
+# *************************************************
+if __name__ == "__main__":
+    app.run(debug=True)
